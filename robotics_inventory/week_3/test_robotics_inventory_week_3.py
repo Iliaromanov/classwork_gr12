37bd87634a1638fafcac14205849393f8e66f29d
Iliaromanov <47427592+Iliaromanov@users.noreply.github.com>
2020-05-01 16:32:21 -0400

    Update test_robotics_inventory_week_3.py

--- robotics_inventory/week_3/test_robotics_inventory_week_3.py
+++ robotics_inventory/week_3/test_robotics_inventory_week_3.py
@@ -1,4 +1,4 @@
-from main import BaseRecord, Bin, InventoryManager, User
+from main import BaseRecord, Bin, InventoryManager, User, Part
 
 def test_find_bin_by_location():
     me = InventoryManager()
@@ -9,6 +9,7 @@ def test_find_bin_by_location():
     assert me.find_bin_by_location("here") == a
     assert me.find_bin_by_location("there") == b
 
+
 def test_find_user_by_student_num():
     me = InventoryManager()
     bill = User("bill.nye20@ycdsbk12.ca", 123456789)
@@ -17,3 +18,16 @@ def test_find_user_by_student_num():
 
     assert me.find_user_by_student_num(123456789) == bill
     assert me.find_user_by_student_num(987654321) == shaquiffa
+
+def test_add_part():
+    me = InventoryManager()
+    a = Bin("here", "adfsf", 5)
+    b = Bin("there", "df", 2)
+    me.add_part("motherboard", 1, "here")
+    me.add_part("ssd", 1, "there")
+
+    for part in me.parts:
+        if part.name == "motherboard":
+            assert int(part.bin_id) == 0
+        elif part.name == "ssd":
+            assert part.bin_id == 1
