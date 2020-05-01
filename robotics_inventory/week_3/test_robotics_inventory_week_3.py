b24aa99d2fff9f7b1d200ca560d1ce4da30555b4
Iliaromanov <47427592+Iliaromanov@users.noreply.github.com>
2020-05-01 00:19:43 -0400

    Update test_robotics_inventory_week_3.py

--- robotics_inventory/week_3/test_robotics_inventory_week_3.py
+++ robotics_inventory/week_3/test_robotics_inventory_week_3.py
@@ -1,7 +1,8 @@
 from main import BaseRecord, Bin, InventoryManager
 
 def test_find_bin_by_location():
+    me = InventoryManager()
     a = Bin("here", "adfsf", 5)
     b = Bin("there", "df", 2)
     c = Bin("not here", "boad", 54)
-    assert find_bin_by_location("here") == a
+    assert me.find_bin_by_location("here") == a
