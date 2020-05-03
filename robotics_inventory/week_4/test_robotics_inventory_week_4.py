dca97109c59c2a4fad12b9e1b5a5b957707c0d5f
Iliaromanov <47427592+Iliaromanov@users.noreply.github.com>
2020-05-03 13:17:22 -0400

    Update test_robotics_inventory_week_4.py

--- robotics_inventory/week_4/test_robotics_inventory_week_4.py
+++ robotics_inventory/week_4/test_robotics_inventory_week_4.py
@@ -1,1 +1,13 @@
 
+from main import BaseRecord, User, InventoryManager, Bin
+
+def test_create_user():
+    me = InventoryManager()
+    me.create_user("dundas.levins20@ycdsbk12.ca", 1112387)
+    assert len(User.all_users) == 1
+
+    me.create_user("bart.garthoplis@ycdsb.ca", 2224241)
+    assert len(User.all_users) == 2
+
+def test_find_bin_by_location():
+    pass
