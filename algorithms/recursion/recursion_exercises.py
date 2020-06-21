d14e4e526b96b0b8e743d9e4a9cec0a01a8de712
Iliaromanov <47427592+Iliaromanov@users.noreply.github.com>
2020-06-21 18:36:25 -0400

    Update recursion_exercises.py

--- algorithms/recursion/recursion_exercises.py
+++ algorithms/recursion/recursion_exercises.py
@@ -314,3 +314,17 @@ def array220(nums: List[int], index: int) -> bool:
         return True
     else:
         return array220(nums[1:], index + 1)
+    
+    
+def allStar(string: str) -> str:
+    """
+    Given a string, compute recursively a new string where all the adjacent chars are now separated by a "*".
+    allStar("hello") → "h*e*l*l*o"
+    allStar("abc") → "a*b*c"
+    allStar("ab") → "a*b"
+    """
+
+    if len(string) <= 1:
+        return string
+
+    return string[0] + '*' + allStar(string[1:])
