e5bc11d7258b1fb743738b7e1110e9c77c05fe67
Iliaromanov <47427592+Iliaromanov@users.noreply.github.com>
2020-06-21 18:19:10 -0400

    Update recursion_exercises.py

--- algorithms/recursion/recursion_exercises.py
+++ algorithms/recursion/recursion_exercises.py
@@ -280,3 +280,20 @@ def array6(nums: List[int], index: int) -> bool:
         return True
     else:
         return array6(nums[1:], index + 1)
+    
+    
+def array11(nums: List[int], index: int) -> int:
+    """
+    Given an array of ints, compute recursively the number of times that the value 11 appears in the array. We'll use the convention of considering only the part of the array that begins at the given index. In this way, a recursive call can pass index+1 to move down the array. The initial call will pass in index as 0.
+    array11([1, 2, 11], 0) → 1
+    array11([11, 11], 0) → 2
+    array11([1, 2, 3, 4], 0) → 0
+    """
+
+    if len(nums) == 0:
+        return 0
+
+    if nums[0] == 11:
+        return 1 + array11(nums[1:], index + 1)
+    else:
+        return array11(nums[1:], index + 1)
